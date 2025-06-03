c29f69c9fdd0ea99007942173f7e416feab2d39a
Tejchak <chakravarthy.t@northeastern.edu>
2025-06-03 11:38:17 +0530

    main nufs file

---
+++ nufs.c
@@ -0,0 +1,216 @@
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "storage.h"
+
+
+#define FUSE_USE_VERSION 26
+#include <fuse.h>
+
+// implementation for: man 2 access
+// Checks if a file exists.
+int nufs_access(const char *path, int mask) {
+  struct stat st;
+  int rv = storage_stat(path, &st);
+  printf("access(%s, %04o) -> %d\n", path, mask, rv);
+  return rv;
+}
+
+// Gets an object's attributes (type, permissions, size, etc).
+// Implementation for: man 2 stat
+// This is a crucial function.
+int nufs_getattr(const char *path, struct stat *st) {
+  int rv = 0;
+
+  if (strcmp(path, "/") == 0) {
+    st->st_mode = 040755; // directory
+    st->st_size = 0;
+    st->st_uid = getuid();
+  } else { 
+    //delegate to storage stat
+    rv = storage_stat(path, st);
+    st->st_uid = getuid();
+  }
+  printf("getattr(%s) -> (%d) {mode: %04o, size: %ld}\n", path, rv, st->st_mode,
+         st->st_size);
+  return rv;
+}
+
+// implementation for: man 2 readdir
+// lists the contents of a directory
+int nufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                 off_t offset, struct fuse_file_info *fi) {
+  struct stat st;
+  int rv;
+  //delegate to storage stat
+  rv = storage_stat(path, &st);
+  assert(rv == 0);
+
+  filler(buf, ".", &st, 0);
+  storage_stat("..", &st); 
+  filler(buf, "..", &st, 0);
+  slist_t *entries = storage_list(path);
+  slist_t *cur = entries;
+
+  while (cur) {
+    const char *name = cur->data;
+    char full_path[128];
+    snprintf(full_path, sizeof(full_path), "%s/%s", path, name);
+    if (strcmp(path, "/") == 0) {
+      snprintf(full_path, sizeof(full_path), "/%s", name); // avoid //
+    }
+
+    if (storage_stat(full_path, &st) == 0) {
+      filler(buf, name, &st, 0);
+    }
+
+    cur = cur->next;
+  }
+  s_free(entries);
+  printf("readdir(%s) -> %d\n", path, rv);
+  return 0;
+}
+
+// mknod makes a filesystem object like a file or directory
+// called for: man 2 open, man 2 link
+// Note, for this assignment, you can alternatively implement the create
+// function.
+int nufs_mknod(const char *path, mode_t mode, dev_t rdev) {
+  int rv = storage_mknod(path, mode);
+  printf("mknod(%s, %04o) -> %d\n", path, mode, rv);
+  return rv;
+}
+
+// same thing as mknod
+int nufs_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+  int rv = storage_mknod(path, mode);
+  printf("create(%s, %04o) -> %d\n", path, mode, rv);
+  return rv;
+}
+
+// most of the following callbacks implement
+// another system call; see section 2 of the manual
+int nufs_mkdir(const char *path, mode_t mode) {
+  int rv = storage_mkdir(path, mode);
+  printf("mkdir(%s) -> %d\n", path, rv);
+  return rv;
+}
+
+//removes files by delegating to storage unlink
+int nufs_unlink(const char *path) {
+  int rv = storage_unlink(path);
+  printf("unlink(%s) -> %d\n", path, rv);
+  return rv;
+}
+
+int nufs_link(const char *from, const char *to) {
+  int rv = -1;
+  printf("link(%s => %s) -> %d\n", from, to, rv);
+  return rv;
+}
+
+//removes a directory by delegating to storage rmdir
+int nufs_rmdir(const char *path) {
+  int rv = storage_rmdir(path);
+  printf("rmdir(%s) -> %d\n", path, rv);
+  return rv;
+}
+
+// implements: man 2 rename
+// called to move a file within the same filesystem
+int nufs_rename(const char *from, const char *to) {
+  int rv = storage_rename(from, to);
+  printf("rename(%s => %s) -> %d\n", from, to, rv);
+  return rv;
+}
+
+int nufs_chmod(const char *path, mode_t mode) {
+  int rv = -1;
+  printf("chmod(%s, %04o) -> %d\n", path, mode, rv);
+  return rv;
+}
+
+//resizes the file by delegating to storage truncate
+int nufs_truncate(const char *path, off_t size) {
+  int rv = storage_truncate(path, size);
+  printf("truncate(%s, %ld bytes) -> %d\n", path, size, rv);
+  return rv;
+}
+
+// This is called on open, but doesn't need to do much
+// since FUSE doesn't assume you maintain state for
+// open files.
+// You can just check whether the file is accessible.
+int nufs_open(const char *path, struct fuse_file_info *fi) {
+  int rv = 0;
+  printf("open(%s) -> %d\n", path, rv);
+  return rv;
+}
+
+// Actually read data
+int nufs_read(const char *path, char *buf, size_t size, off_t offset,
+              struct fuse_file_info *fi) {
+  int rv = storage_read(path, buf, size, offset);
+  printf("read(%s, %ld bytes, @+%ld) -> %d\n", path, size, offset, rv);
+  return rv;
+}
+
+// Actually write data
+int nufs_write(const char *path, const char *buf, size_t size, off_t offset,
+               struct fuse_file_info *fi) {
+  int rv = storage_write(path, buf, size, offset);
+  printf("write(%s, %ld bytes, @+%ld) -> %d\n", path, size, offset, rv);
+  return rv;
+}
+
+// Update the timestamps on a file or directory.
+int nufs_utimens(const char *path, const struct timespec ts[2]) {
+  //don't really see a reason to this so I just have return true (0) so touch works
+  (void)path; 
+  (void)ts;
+  return 0;
+}
+
+// Extended operations
+int nufs_ioctl(const char *path, int cmd, void *arg, struct fuse_file_info *fi,
+               unsigned int flags, void *data) {
+  int rv = -1;
+  printf("ioctl(%s, %d, ...) -> %d\n", path, cmd, rv);
+  return rv;
+}
+
+void nufs_init_ops(struct fuse_operations *ops) {
+  memset(ops, 0, sizeof(struct fuse_operations));
+  ops->access = nufs_access;
+  ops->getattr = nufs_getattr;
+  ops->readdir = nufs_readdir;
+  ops->mknod = nufs_mknod;
+  ops->create   = nufs_create; // alternative to mknod
+  ops->mkdir = nufs_mkdir;
+  ops->link = nufs_link;
+  ops->unlink = nufs_unlink;
+  ops->rmdir = nufs_rmdir;
+  ops->rename = nufs_rename;
+  ops->chmod = nufs_chmod;
+  ops->truncate = nufs_truncate;
+  ops->open = nufs_open;
+  ops->read = nufs_read;
+  ops->write = nufs_write;
+  ops->utimens = nufs_utimens;
+  ops->ioctl = nufs_ioctl;
+};
+
+struct fuse_operations nufs_ops;
+
+//main file to run everything
+int main(int argc, char *argv[]) {
+  assert(argc > 2 && argc < 6);
+  storage_init(argv[--argc]);
+  nufs_init_ops(&nufs_ops);
+  return fuse_main(argc, argv, &nufs_ops, NULL);
+}
